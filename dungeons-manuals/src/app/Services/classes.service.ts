6ea45e914c8d03d7aba3026d9049048052176c1f
Mauro-Cassoni <mauro.cassoni.94@gmail.com>
2023-12-18 17:17:23 +0100

    classes

--- dungeons-manuals/src/app/Services/classes.service.ts
+++ dungeons-manuals/src/app/Services/classes.service.ts
@@ -1,9 +1,22 @@
+import { IClass } from './../Models/i-class';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IRef } from '../Models/i-ref';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClassesService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  classes:IClass[] = [];
+
+  genericUrl:string = environment.api_url
+
+  getClasses():Observable<IRef[]> {
+    return this.http.get<IRef[]>(`${this.genericUrl}/classes`)
+  }
 }
