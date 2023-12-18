831f23656aef8c523d20f4a6f1fd78d87633ee81
Maudit00 <davide9311@gmail.com>
2023-12-18 16:38:00 +0100

    classes module done

--- dungeons-manuals/src/app/app-routing.module.ts
+++ dungeons-manuals/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, { path: 'classes', loadChildren: () => import('./pages/classes/classes.module').then(m => m.ClassesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
