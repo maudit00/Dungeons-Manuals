1c8abde53e9e23a6fdd496f0b3cc3e6ccdff758d
Emilio-Plances <emilio.plances@gmail.com>
2023-12-19 09:47:10 +0100

    monter page created

--- dungeons-manuals/src/app/app-routing.module.ts
+++ dungeons-manuals/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, { path: 'monster', loadChildren: () => import('./pages/monster/monster.module').then(m => m.MonsterModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
