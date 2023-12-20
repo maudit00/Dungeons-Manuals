a7faebf0fa5301cc8c603fdd69f347ad29f0627f
Maudit00 <davide9311@gmail.com>
2023-12-20 15:48:25 +0100

    races module filled

--- dungeons-manuals/src/app/pages/races/races.component.ts
+++ dungeons-manuals/src/app/pages/races/races.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RacesService } from '../../Services/races.service';
+import { IRef } from '../../Models/i-ref';
 
 @Component({
   selector: 'app-races',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './races.component.scss'
 })
 export class RacesComponent {
+races:IRef[] = []
 
+constructor (private racesSvc:RacesService){
+this.getRaces()
+}
+
+getRaces() {
+	this.racesSvc.getRaces().subscribe(races => {
+		this.races = races.results
+		console.log (this.races, races.results)
+	})
+}
 }
