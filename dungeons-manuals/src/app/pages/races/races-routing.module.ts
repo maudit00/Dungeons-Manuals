be37615cf80d13f85d1c388a575e7e612b7355dd
Maudit00 <davide9311@gmail.com>
2023-12-20 18:03:17 +0100

    race component layout done has to be filled

--- dungeons-manuals/src/app/pages/races/races-routing.module.ts
+++ dungeons-manuals/src/app/pages/races/races-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RacesComponent } from './races.component';
-
-const routes: Routes = [{ path: '', component: RacesComponent }];
+import { RaceComponent } from '../../shared/components/race/race.component';
+const routes: Routes = [
+	{
+	 path: '',
+	 component: RacesComponent
+	},
+	{
+	 path:':index',
+	 component: RaceComponent
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
