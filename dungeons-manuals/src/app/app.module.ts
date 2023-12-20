2de637d024f27397d378dcc9b2435847a4377910
Mauro-Cassoni <mauro.cassoni.94@gmail.com>
2023-12-20 12:04:40 +0100

    component footer creato

--- dungeons-manuals/src/app/app.module.ts
+++ dungeons-manuals/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
