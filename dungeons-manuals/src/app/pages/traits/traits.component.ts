5422a5fe8e00eff3386715c773f1a6c0719c0588
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2023-12-20 19:03:00 +0100

    traits module create

--- dungeons-manuals/src/app/pages/traits/traits.component.ts
+++ dungeons-manuals/src/app/pages/traits/traits.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IResult } from '../../models/i-traits';
+import { TraitsService } from '../../services/traits.service';
 
 @Component({
   selector: 'app-traits',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class TraitsComponent {
 
+  traits:IResult[]=[]
+
+  constructor( private traitsSvc:TraitsService){}
+
+  ngOnInit(){
+    this.traitsSvc.getTraits().subscribe( t =>{
+      this.traits = t.results;
+      console.log(this.traits);
+    })
+  }
 }
